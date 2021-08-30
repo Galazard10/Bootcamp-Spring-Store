70211c2c8727882c03c624f945c69038c2a83fcd
Galazard10 <galym.ilyas.2002@gmail.com>
2021-08-31 04:30:59 +0600

    Sixth Commit on Spring Store Project: Mulltilanguage mode done: Russian language added.

---
+++ src/main/java/kz/bootcamp/springBoot/SpringBoot/config/WebMVCConfig.java
@@ -0,0 +1,44 @@
+package kz.bootcamp.springBoot.SpringBoot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setDefaultEncoding("UTF-8");
+        source.setBasename("classpath:translations");
+        return source;
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieMaxAge(3600*24*365);
+        resolver.setCookieName("languageValue");
+        resolver.setDefaultLocale(new Locale("ru"));
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lng");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+    }
+}
